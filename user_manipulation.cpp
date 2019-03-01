79d57095312a229814551b6716aa3cd24a386401
MatheusClaud <soueuounaoquemsabe@gmail.com>
2019-02-28 23:46:42 -0300

    add user function added

--- user_manipulation.cpp
+++ user_manipulation.cpp
@@ -7,17 +7,54 @@ using namespace std;
 
 // databank area
 
-// main functions ---------------------------------------------------
+// user model
+struct user{
+  vector <int> friendsId;
+  char name;
+  char random_thing;
+};
+
+// processament area
+bool goback = true;
+
+// users storage / capacity: 50 users
+user users_storage[49];
+int last_user = -1; // last user space used / -1: empty
+
+// user functions ------------------------------------------------------------
+
+void make_user()
+{
+    cout << endl;
+
+    if (last_user < 50){
+      char name;
+      char random;
+
+      cout << "please write a name" << endl;
+      cin >> name;
+
+      cout << "tell some letter" << endl;
+      cin >> random;
+
+      users_storage[last_user+1].name = name;
+      users_storage[last_user+1].random_thing = random;
+
+    }
+    else{
+      cout << "user-storage-error-overflow";
+    }
+}
+
+// main functions ------------------------------------------------------------
 
 // a function to read user option and redirect to next step
-
 void get_options()
 {
   int option;
   cin >> option;
 
   //redirect
-
   if (option == 1)
   {
     make_user();
@@ -32,17 +69,21 @@ void get_options()
   }
 }
 
-// a function to shoe main show_main_menu
-
+// a function to show main menu
 void show_main_menu()
 {
     // show main menu
+    cout << endl;
     cout << "1 - Add user" << endl;
     cout << "2 - Make friendship" << endl;
     cout << "3 - Show user" << endl;
 
     // redirect
     get_options();
+
+    if (goback){
+      show_main_menu();
+    }
 
 }
 
